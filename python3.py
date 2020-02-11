5c03cf2b41db5df639f2525210afd03890f34a58
gayu-ramesh <60912887+gayu-ramesh@users.noreply.github.com>
2020-02-10 23:03:57 -0800

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,8 @@
+num=int(input("enter a no"))
+num1=int(input("enter a no"))
+operation=input("enter the operation")
+if operation=='add':
+    print("the sum is ", num+num1)
+elif operation=='sub':
+    print("the diff is ", num-num1)
+
